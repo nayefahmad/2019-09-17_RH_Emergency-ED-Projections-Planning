503e9fd9acaa35450c33ef985dee719781c89c1e
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-09-18 15:18:27 -0700

    NEW: R and Rproj

---
+++ src/2019-09-17_RH_Emergency-ED-Projections-Planning.R
@@ -0,0 +1,23 @@
+
+#'--- 
+#' title: "RHS ED projections"
+#' author: "Nayef Ahmad"
+#' date: "2019-07-18"
+#' output: 
+#'   html_document: 
+#'     keep_md: yes
+#'     code_folding: show
+#'     toc: true
+#'     toc_float: true
+#' ---
+#' 
+
+#+ lib, include = FALSE 
+library(tidyverse)
+library(denodoExtractor)
+
+setup_denodo()
+
+#+ data 
+# pull rhs ed visit data: ----------------
+
