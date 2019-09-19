b40fd2a720eaa7192143a8c6ec583dc1da5f48ee
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-09-18 18:45:06 -0700

    move plotting function to new script

--- src/2019-09-17_RH_Emergency-ED-Projections-Planning.R
+++ src/2019-09-17_RH_Emergency-ED-Projections-Planning.R
@@ -26,6 +26,11 @@ ed_mart <- dplyr::tbl(cnx, dbplyr::in_schema("EDMart.dbo",
 
 people_2018 <- dplyr::tbl(cnx, dbplyr::in_schema("DSSI.dbo", 
                                                  "[PEOPLE2018Complete]"))
+
+# functions: 
+source(here::here("src", 
+                  "plot-pop-vs-year-for-pop-segment_function.R"))
+
 #' ## ED visits data 
 #+ data 
 # pull rhs ed visit data: ----------------
@@ -132,40 +137,6 @@ df3.pop_nested <-
   group_by(age_group) %>% 
   nest()
 
-# plotting function: 
-plot_trend <- function(df, 
-                       subset = NULL){
-  # arguments: 
-  # df: df with cols: year, pop
-  # subset: optional, used to specify which subset of population we're 
-  #   focusing on. E.g. "65-69 years old"
-  
-  # returns ggplot with trend line
-  
-  df %>% 
-    ggplot(aes(x = year, 
-               y = pop)) + 
-    geom_point() + 
-    geom_line() + 
-    scale_y_continuous(limits = c(0, 500000), 
-                       breaks = seq(0, 500000, 50000), 
-                       labels = sprintf("%i K", seq(0, 500, 50))) + 
-    labs(title = sprintf("%s : Population growth for all of BC", 
-                         subset), 
-         caption = "Data source: PEOPLE 2018, BC Stats \nRetrieved from: [DSSI].[dbo].[PEOPLE2018Complete]", 
-         subtitle = "2010 to 2036") + 
-    theme_light() +
-    theme(panel.grid.minor = element_line(colour = "grey95"), 
-          panel.grid.major = element_line(colour = "grey95"))
-        
-  
-}
-
-
-# test function: 
-# First run the following script until definition of `df3.pop_nested`: 
-# "src/2019-09-17_RH_Emergency-ED-Projections-Planning.R"
-# df3.pop_nested$data[[1]] %>% plot_trend()
 
 # map plotting function across all age groups: 
 df3.pop_nested <- 
